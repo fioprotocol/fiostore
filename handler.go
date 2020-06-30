360df28a53679f82a1c02c41b0987f3a52663b38
Todd G <todd@dapix.io>
2020-06-29 19:45:40 -0600

    more informative errors

--- handler.go
+++ handler.go
@@ -11,7 +11,7 @@ func ReqHandler(resp http.ResponseWriter, req *http.Request) {
 
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		log.Printf("%s: %v\n", req.RemoteAddr, err)
+		log.Printf("%s: unable to read body, sending blank response. %v\n", req.RemoteAddr, err)
 		resp.WriteHeader(400)
 		resp.Write(nil)
 		return
@@ -22,13 +22,14 @@ func ReqHandler(resp http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Printf("%s: %v\n", req.RemoteAddr, err)
 		if status == nil {
+			log.Printf("%s: parse request returned nil status! sending blank response\n", req.RemoteAddr)
 			resp.WriteHeader(500)
 			resp.Write(nil)
 			return
 		}
 		j, err := json.MarshalIndent(status, "", "  ")
 		if err != nil {
-			log.Printf("%s: %v\n", req.RemoteAddr, err)
+			log.Printf("%s: parse request returned invalid status struct! sending blank response %v\n", req.RemoteAddr, err)
 			resp.WriteHeader(500)
 			resp.Write(nil)
 			return
@@ -43,6 +44,7 @@ func ReqHandler(resp http.ResponseWriter, req *http.Request) {
 		log.Printf("%s: %v\n", req.RemoteAddr, err)
 	}
 	if result == nil {
+		log.Printf("%s: request to %s, send request provided nil result! sending empty response\n", req.RemoteAddr, fioRequest.FioAddress)
 		resp.WriteHeader(500)
 		resp.Write(nil)
 		return
@@ -56,7 +58,7 @@ func ReqHandler(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 	if result.Code == 200 {
-		log.Printf("%s: sent request to %s with txid %s\n", req.RemoteAddr, fioRequest.FioAddress, result.Txid)
+		log.Printf("%s: SUCCESS: sent request to %s with txid %s\n", req.RemoteAddr, fioRequest.FioAddress, result.Txid)
 	} else {
 		log.Printf("%s: request to %s failed %s\n", req.RemoteAddr, fioRequest.FioAddress, result.Message)
 	}
